f368ac295293d1e5b34575bab5f7e2d191be39db
liukl <liukl@ucap.com.cn>
2018-03-09 15:24:51 +0800

    成功将项目放在tomcat中执行起来

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 //表明这是一个 Controller
 @Controller
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 	//设置访问的url
 	@RequestMapping("/")
 	//表示返回JSON格式的结果，如果前面使用的是@RestController可以不用写
@@ -16,6 +17,13 @@ public class DemoApplication {
 	String home() {
 		return "Hello World!";//返回结果为字符串
 	}
+
+	@RequestMapping("/demo")
+	@ResponseBody
+	public String demo(){
+		return "demo";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
